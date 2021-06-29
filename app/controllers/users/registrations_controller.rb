e5018e2ea9c31174c899bf4fb56deea28f2f8aea
Daniel <daniel_ansari@sennalabs.com>
2021-06-29 15:18:26 +0700

    fix: error in after register

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_inactive_sign_up_path_for(resource)
-    after_register_path(:add_zip)
+    after_register_path(:job)
   end
-  
+
   def after_sign_up_path_for(resource)
-    after_register_path(:add_zip)
+    after_register_path(:job)
   end
 end
