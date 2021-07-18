5967336812f1091309ae5a25e54eeb1eae072261
Daniel <daniel_ansari@sennalabs.com>
2021-07-18 07:20:47 +0700

    fix: popup portfolio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,11 @@ module ApplicationHelper
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 
-  def thumb_url(image)
-    image.variant(gravity: "center", auto_orient: true, rotate: 0, resize: "200x200^", crop: "200x200+0+0")
+  def thumb_url(image, resize = "200x200^", crop = "200x200+0+0")
+    image.variant(gravity: "center", auto_orient: true, rotate: 0, resize: resize, crop: crop)
+  end
+
+  def picture_only_url(image, resize = "499x499^", crop = "499x499+0+0")
+    Rails.application.routes.url_helpers.rails_representation_url(thumb_url(image, resize, crop).processed, only_path: true)
   end
 end
