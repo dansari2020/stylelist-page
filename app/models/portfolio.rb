4a6c82721b0d399dbc7b111ad6d761a3d6bf7157
Daniel <daniel_ansari@sennalabs.com>
2021-07-21 11:52:00 +0700

    fix: porfolio mobil view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,6 +8,8 @@ class Portfolio < ApplicationRecord
   has_many_attached :pictures
   accepts_nested_attributes_for :service_types, :hair_types, allow_destroy: true
 
+  delegate :first_name, to: :user, prefix: true
+
   def self.hair_length_list
     hair_lengths.keys.map { |g| [g.humanize, g] }
   end
