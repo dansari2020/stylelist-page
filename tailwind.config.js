5aa20a02bcad6f8242c0a5f17cb75ee744ecb0b3
Daniel <daniel_ansari@sennalabs.com>
2021-07-28 04:20:14 +0700

    fix: css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  purge: [
-    './app/**/*.html',
-    './app/**/*.erb',
-    './app/**/*.jsx',
-  ],
+  purge: ["./app/**/*.html", "./app/**/*.erb", "./app/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      border: {
+        0.5: "0.5px",
+      },
       width: {
-        '15': '3.75rem',
-        '17': '68px',
-        '50': '200px',
+        15: "3.75rem",
+        17: "68px",
+        50: "200px",
       },
       height: {
-        '15': '3.75rem',
-        '17': '68px',
-        '50': '200px',
+        15: "3.75rem",
+        17: "68px",
+        18: "4.5rem",
+        50: "200px",
       },
       padding: {
-        '3.7': '0.938rem',
-      }
+        3.7: "0.938rem",
+      },
     },
   },
   variants: {
     extend: {},
-    display: ['responsive', 'group-hover', 'group-focus'],
+    display: ["responsive", "group-hover", "group-focus"],
   },
   plugins: [],
-}
+};
