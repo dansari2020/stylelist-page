a1eaa8cb7e7063a54a810c00ef81ab8de66df0e0
Daniel <d0nsari2020@gmail.com>
2021-08-21 18:14:45 +0700

    fix css modal

--- app/models/user.rb
+++ app/models/user.rb
@@ -133,7 +133,7 @@ class User < ApplicationRecord
       end
     end
 
-    components.join(", ")
+    components.empty? ? "Less than one month" : components.join(", ")
   end
 
   # def active_for_authentication?
