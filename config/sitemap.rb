a316875e44ed1c4a8cfd7549dfe100cc98f250c4
Daniel <d0nsari2020@gmail.com>
2021-09-23 04:09:48 +0700

    fix: sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 SitemapGenerator::Sitemap.default_host = ENV["WEBSITE_URL"]
-SitemapGenerator::Sitemap.ping_search_engines("#{ENV["WEBSITE_URL"]}/sitemap.xml.gz")
+SitemapGenerator::Sitemap.ping_search_engines("#{ENV["WEBSITE_URL"]}sitemap.xml.gz")
 SitemapGenerator::Sitemap.create do
   add root_path
   add new_user_registration_path
