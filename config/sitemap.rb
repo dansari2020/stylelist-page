128617601b9aebf6ddc245dfc5301caa3c602886
Daniel <d0nsari2020@gmail.com>
2021-09-23 22:25:44 +0700

    fix: หระำทฟย

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
-SitemapGenerator::Sitemap.default_host = ENV["WEBSITE_URL"]
-SitemapGenerator::Sitemap.ping_search_engines("#{ENV["WEBSITE_URL"]}sitemap.xml.gz")
+SitemapGenerator::Sitemap.default_host = "https://stylistpage.herokuapp.com"
+SitemapGenerator::Sitemap.ping_search_engines("https://stylistpage.herokuapp.com/sitemap.xml.gz")
 SitemapGenerator::Sitemap.create do
   add root_path
   add new_user_registration_path
