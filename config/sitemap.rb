a4ecdd87d32708f014d8535e5f5dc49aefef9c60
Daniel <d0nsari2020@gmail.com>
2021-09-24 23:20:48 +0700

    fix: add sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,8 +1,10 @@
-SitemapGenerator::Sitemap.default_host = "https://stylistpage.herokuapp.com"
-SitemapGenerator::Sitemap.ping_search_engines("https://stylistpage.herokuapp.com/sitemap.xml.gz")
+SitemapGenerator::Sitemap.default_host = "https://stylistpage.com"
+SitemapGenerator::Sitemap.ping_search_engines("https://stylistpage.com/sitemap.xml.gz")
+SitemapGenerator::Sitemap.create_index = true
 SitemapGenerator::Sitemap.create do
-  add root_path
-  add new_user_registration_path
-  add new_user_session_path
+  add "/auth/sign_in"
+  add "/auth/sign_up"
+  add "/terms_of_service"
+  add "/privacy_policy"
 end
 
