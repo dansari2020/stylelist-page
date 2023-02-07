4f4049218480e2d91e381e4f525fe843f26d54e8
Daniel Ansari <d0nsari2020@gmail.com>
2023-02-08 01:13:53 +0700

    add lazy loading

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_view.image_loading = "lazy"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
